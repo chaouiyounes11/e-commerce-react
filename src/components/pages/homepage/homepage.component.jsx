08ee84b3a7143bb4e99999902cc08c9238a70fec
Younes <younes@MacBook-Pro-de-Administrateur.local>
2020-03-26 12:20:21 +0100

    broken HP into smaller components, finished styling menu item component

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../directory/directory.component'
+
+const HomePage = () => (
+    <Directory />
+)
+
+
+export default HomePage
